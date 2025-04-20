1c1bf4af21d330d60fd2e7edc2b50ce484a46d61
Carnage <akotoxmpimbo@gmail.com>
2025-04-20 19:43:47 +0200

    feat: remove unused email sending logic and mock order data from MyOrdersPage

--- app/(customer)/history/page.tsx
+++ app/(customer)/history/page.tsx
@@ -1,146 +1,7 @@
-// "use client"
-
-// import { emailOrderHistory } from "@/app/actions/orders"
-// import { Button } from "@/components/ui/button"
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardFooter,
-//   CardHeader,
-//   CardTitle,
-// } from "@/components/ui/card"
-// import { Input } from "@/components/ui/input"
-// import { Label } from "@/components/ui/label"
-// import { useFormState, useFormStatus } from "react-dom"
-
-// export default function MyOrdersPage() {
-//   const [data, action] = useFormState(emailOrderHistory, {})
-//   return (
-//     <form action={action} className="max-2-xl mx-auto">
-//       <Card>
-//         <CardHeader>
-//           <CardTitle>My Orders</CardTitle>
-//           <CardDescription>
-//             Enter your email and we will email you your order history and
-//             download links
-//           </CardDescription>
-//         </CardHeader>
-//         <CardContent>
-//           <div className="space-y-2">
-//             <Label htmlFor="email">Email</Label>
-//             <Input type="email" required name="email" id="email" />
-//             {data.error && <div className="text-destructive">{data.error}</div>}
-//           </div>
-//         </CardContent>
-//         <CardFooter>
-//           {data.message ? <p>{data.message}</p> : <SubmitButton />}
-//         </CardFooter>
-//       </Card>
-//     </form>
-//   )
-// }
-
-// function SubmitButton() {
-//   const { pending } = useFormStatus()
-
-//   return (
-//     <Button className="w-full" size="lg" disabled={pending} type="submit">
-//       {pending ? "Sending..." : "Send"}
-//     </Button>
-//   )
-// }
-
-// "use client";
 
 import OrderActions from "@/components/order-confirmation";
-import { Button } from "@/components/ui/button";
-import db from "@/db/db";
-import { getUserOrders, OrderWithProduct } from "@/server/client/getUserOrser";
-import { sendConfirmationEmail } from "@/server/client/sendLink";
-import { auth, currentUser } from "@clerk/nextjs/server";
-import { Resend } from "resend";
-
-// export const mockOrders = [
-//   {
-//     id: "order-1",
-//     pricePaidInCents: 1499,
-//     createdAt: new Date().toISOString(),
-//     product: {
-//       id: "prod-1",
-//       name: "Next.js Crash Course",
-//       imagePath: "/products/nextjs-course.jpg",
-//       description: "Learn the basics of Next.js and build your first app.",
-//     },
-//   },
-//   {
-//     id: "order-2",
-//     pricePaidInCents: 2999,
-//     createdAt: new Date().toISOString(),
-//     product: {
-//       id: "prod-2",
-//       name: "Advanced Tailwind Workshop",
-//       imagePath: "/products/tailwind-workshop.jpg",
-//       description: "Master Tailwind CSS with real-world examples.",
-//     },
-//   },
-//   {
-//     id: "order-3",
-//     pricePaidInCents: 1999,
-//     createdAt: new Date().toISOString(),
-//     product: {
-//       id: "prod-3",
-//       name: "TypeScript for Beginners",
-//       imagePath: "/products/typescript-course.jpg",
-//       description: "Get started with TypeScript and write better JavaScript.",
-//     },
-//   },
-//   {
-//     id: "order-4",
-//     pricePaidInCents: 1999,
-//     createdAt: new Date().toISOString(),
-//     product: {
-//       id: "prod-3",
-//       name: "TypeScript for Beginners",
-//       imagePath: "/products/typescript-course.jpg",
-//       description: "Get started with TypeScript and write better JavaScript.",
-//     },
-//   },
-//   {
-//     id: "order-5",
-//     pricePaidInCents: 1999,
-//     createdAt: new Date().toISOString(),
-//     product: {
-//       id: "prod-3",
-//       name: "TypeScript for Beginners",
-//       imagePath: "/products/typescript-course.jpg",
-//       description: "Get started with TypeScript and write better JavaScript.",
-//     },
-//   },
-//   {
-//     id: "order-6",
-//     pricePaidInCents: 1999,
-//     createdAt: new Date().toISOString(),
-//     product: {
-//       id: "prod-3",
-//       name: "TypeScript for Beginners",
-//       imagePath: "/products/typescript-course.jpg",
-//       description: "Get started with TypeScript and write better JavaScript.",
-//     },
-//   },
-// ];
-
-// type Order = {
-//   id: string;
-//   pricePaidInCents: number;
-//   createdAt: string | Date;
-//   product: {
-//     id: string;
-//     name: string;
-//     imagePath: string;
-//     description: string;
-//   };
-// };
+import { getUserOrders } from "@/server/client/getUserOrser";
+import { auth } from "@clerk/nextjs/server";
 
 export default async function MyOrdersPage() {
   const { userId, redirectToSignIn } = await auth();
@@ -148,24 +9,6 @@ export default async function MyOrdersPage() {
   if (userId == null) return redirectToSignIn();
 
   const orders = await getUserOrders(userId);
-
-  const handleSend = async (order: OrderWithProduct) => {
-    try {
-      const result = await sendConfirmationEmail({
-        customerEmail: order.email,
-        firstName: "BookWorm",
-        downloadUrl: order.product.filePath,
-      });
-  
-      if (result.success) {
-        console.log("Email sent successfully!");
-      } else {
-        console.error("Failed to send email:", result.error);
-      }
-    } catch (error) {
-      console.error("Unexpected error:", error);
-    }
-  };
 
   return (
     <div className="max-w-6xl mx-auto px-4 py-16 pt-24">
@@ -197,7 +40,6 @@ export default async function MyOrdersPage() {
             <div className="flex flex-col items-start md:items-end gap-1">
               <OrderActions order={order} />
 
-              {/* <p className="text-xs text-green-600">status</p> */}
             </div>
           </li>
         ))}
